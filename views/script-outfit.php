cc58008271e0e0c05b0b9e008225b1d3f82da0e7
Henning Tirre <henning@leuschner-marketing.com>
2018-01-25 11:46:08 +0100

    Updated script view

--- views/script-outfit.php
+++ views/script-outfit.php
@@ -1,5 +1,5 @@
-<?php if($sid): ?>
-    <script class="widget-<?php echo $sid; ?>" src="<?php echo DLM_URL ?>/e/<?php echo $sid ?>/widget.js" async></script>
+<?php if($sid && $style): ?>
+    <script class="widget-<?php echo $sid; ?>" src="<?php echo DLM_URL ?>/e/<?php echo $sid ?>/widget.js?style=<?php echo $style ?>" async></script>
 <?php else: ?>
     <pre>
         <?php echo __('Unfortunately, no ID was passed. Please check the settings of your DressLikeMe plugin.', DLM_TD); ?>
